4872483a6d9b959df217d7dd4542f5af3c4d18fd
Izaak Baker <izaak.emerson2@gmail.com>
2026-06-17 23:25:25 -0400

    Game modes

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,4 +1,11 @@
-import { FaGear, FaDatabase, FaPlay, FaCircleQuestion } from "react-icons/fa6";
+import {
+  FaGear,
+  FaDatabase,
+  FaPlay,
+  FaStop,
+  FaCircleQuestion,
+} from "react-icons/fa6";
+import { useGameStore } from "../state/gameState.ts";
 import { GrScorecard } from "react-icons/gr";
 import { Link, Route, Switch, useLocation } from "wouter";
 import Configure from "./Configure.tsx";
@@ -11,18 +18,35 @@ const ICON_SIZE = 24;
 
 const App = () => {
   const [location] = useLocation();
+  const gameMode = useGameStore((state) => state.gameMode);
+  const stopGame = useGameStore((state) => state.stopGame);
 
   return (
     <div className="flex flex-col items-stretch h-full">
       <div className="h-16 bg-neutral-800 flex items-center pl-4 pr-4 text-white shrink-0">
         <div className="text-4xl pb-2 font-bold text-violet-400">jijo</div>
         <div className="top-menu ml-auto flex gap-4 text-neutral-400">
-          <Link to="/">
-            <FaPlay
-              size={ICON_SIZE}
-              className={location === "/" ? "text-white" : ""}
-            />
-          </Link>
+          {gameMode === "endless" ? (
+            <Link to="/">
+              <FaStop
+                size={ICON_SIZE}
+                className="text-white"
+                onClick={(e) => {
+                  if (location === "/") {
+                    e.preventDefault();
+                  }
+                  stopGame();
+                }}
+              />
+            </Link>
+          ) : (
+            <Link to="/">
+              <FaPlay
+                size={ICON_SIZE}
+                className={location === "/" ? "text-white" : ""}
+              />
+            </Link>
+          )}
           <Link to="/history">
             <GrScorecard
               size={ICON_SIZE}
